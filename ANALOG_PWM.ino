1368c66064b1bfbaf6c79437a22cdb44fb2900c8
Lenilson A.S <savelino852@gmail.com>
2025-05-29 14:10:00 -0300

    Add files via upload

---
+++ ANALOG_PWM.ino
@@ -0,0 +1,12 @@
+extern "C" { void Main(void); }
+void setup               (void) 
+{
+
+  while                  (true)
+  {
+      Main               (   );
+  }
+
+}
+void loop                (void) 
+{                             }
